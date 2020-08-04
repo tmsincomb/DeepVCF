44a4818af2ebdf2ee5862ee6a4821aec450fef67
Troy Sincomb <troysincomb@gmail.com>
2020-08-03 20:16:39 -0700

    pathing helper

---
+++ vcf_correctness/tools.py
@@ -0,0 +1,34 @@
+"""
+Home for string tools and any other misc tool until its large enough for it's own file
+"""
+from typing import Union
+from pathlib import Path
+
+
+def pathing(path: str, new: bool = False) -> Path:
+    """ Guarantees correct expansion rules for pathing.
+
+    :param Union[str, Path] path: path of folder or file you wish to expand.
+    :param bool new: will check if distination exists if new  (will check parent path regardless).
+    :return: A pathlib.Path object.
+
+    >>> pathing('~/Desktop/folderofgoodstuffs/')
+    /home/user/Desktop/folderofgoodstuffs
+    """
+    path = Path(path)
+    # Expand shortened path
+    if str(path)[0] == '~':
+        path = path.expanduser()
+    # Exand local path
+    else:
+        path = path.absolute()
+    # Making sure new paths don't exist while also making sure existing paths actually exist.
+    if new:
+        if not path.parent.exists():
+            raise ValueError(f'ERROR ::: Parent directory of {path} does not exist.')
+        if path.exists():
+            raise ValueError(f'ERROR ::: {path} already exists!')
+    else:
+        if not path.exists():
+            raise ValueError(f'ERROR ::: Path {path} does not exist.')
+    return path
